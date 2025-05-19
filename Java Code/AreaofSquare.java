aaaef7fa15e102b88221f73ae9957263b868fb3e
Gunjan Katre <gunjankatre55@gmail.com>
2025-05-19 15:02:22 +0530

    Add files via upload

---
+++ Java Code/AreaofSquare.java
@@ -0,0 +1,12 @@
+
+public class AreaofSquare 
+{
+
+	public static void main(String[] args) 
+	{
+		int num=4;
+		int area=num*num;
+		System.out.println("Area of Square " +num+ " is " +area);
+	}
+
+}
---
+++ Java Code/LiterToMl.java
@@ -0,0 +1,12 @@
+
+public class LiterToMl 
+{
+
+	public static void main(String[] args) 
+	{
+		int liter=4;
+		int ml=liter*1000;
+		System.out.println(ml);
+	}
+
+}
